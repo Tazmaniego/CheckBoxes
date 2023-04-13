79fcd2bdfc3e578a7cca22e6be36d63d00654c77
Tazmaniego <DiegoAbrahamMontiel@gmail.com>
2023-04-12 19:00:10 -0700

    ;

--- test/specs/Checkbox.spec.js
+++ test/specs/Checkbox.spec.js
@@ -2,10 +2,10 @@ import CheckboxesPage from "../pageobjects/Checkbox.page.js";
 
 describe ('My Checkbox Page', () => {
     it('should select input', async () => {
-        await CheckboxesPage.open()
-        await browser.pause(3000)
-        await (CheckboxesPage).Checkbox1.click()
-        await (CheckboxesPage).Checkbox2.click()
-        await browser.pause(3000)
+        await CheckboxesPage.open();
+        await browser.pause(3000);
+        await (CheckboxesPage).Checkbox1.click();
+        await (CheckboxesPage).Checkbox2.click();
+        await browser.pause(3000);
     })
 })
